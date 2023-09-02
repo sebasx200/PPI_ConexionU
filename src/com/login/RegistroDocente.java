c807212edfeababe8f8e2af19ea9d05cdfc02bbd
sebasx200 <sebasx200@hotmail.com>
2023-09-01 19:28:01 -0500

    Se finaliza la estructura del formulario de inicio de sesión en cuanto a lo gráfico.

--- src/com/login/RegistroDocente.java
+++ src/com/login/RegistroDocente.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package ppi_conexionu;
+package com.login;
 
 /**
  *
  * @author Daniel Henao
  */
-public class RegistrlDocente extends javax.swing.JFrame {
+public class RegistroDocente extends javax.swing.JFrame {
 
     /**
      * Creates new form RegistrlDocente
      */
-    public RegistrlDocente() {
+    public RegistroDocente() {
         initComponents();
     }
 
@@ -254,20 +254,21 @@ public class RegistrlDocente extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RegistrlDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RegistrlDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RegistrlDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RegistrlDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new RegistrlDocente().setVisible(true);
+                new RegistroDocente().setVisible(true);
             }
         });
     }
