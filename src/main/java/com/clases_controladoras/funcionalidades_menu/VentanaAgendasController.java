772385e87462f2caaa94b9231defeff4b19a58f8
sebasx200 <sebasx200@hotmail.com>
2023-11-13 13:24:11 -0500

    Se cambia la manera en que se le pide al usuario que dé las agendas para las asesorías

--- src/main/java/com/clases_controladoras/funcionalidades_menu/VentanaAgendasController.java
+++ src/main/java/com/clases_controladoras/funcionalidades_menu/VentanaAgendasController.java
@@ -2,9 +2,8 @@ package com.clases_controladoras.funcionalidades_menu;
 
 import com.clases.Mensajes;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.CheckBox;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -12,31 +11,53 @@ import java.time.temporal.TemporalAdjusters;
 
 public class VentanaAgendasController {
     @FXML
-    private HBox hBox;
-    @FXML
-    private CheckBox lunes, martes, miercoles, jueves, viernes, sabado, domingo;
-
+    private ComboBox<String> horaInicial, horaFinal, comboDias;
+    public void initialize(){
+        llenarHoras();
+        diaSemana();
+    }
     @FXML
     private void onBotonConfirmarAction(){
-        obtenerDiasSeleccionados();
+        String dia = comboDias.getValue();
+        String horaInicio = horaInicial.getValue();
+        String horaFin = horaFinal.getValue();
+        Mensajes.mensajeConfirmacion("Día y horas seleccionadas",
+                "El día " + dia + " dará asesorías entre las horas " + horaInicio + "-" + horaFin,
+                "¿Está seguro/a de la información proporcianda?");
     }
-    private void obtenerDiasSeleccionados(){
-        boolean seSelecciona = false;
-        String[] dias = new String[7];
-        int pos = 0;
-        for(Node nodo:hBox.getChildren()){
-            if(nodo instanceof CheckBox){
-                CheckBox checkBox = (CheckBox) nodo;
-                if(checkBox.isSelected()){
-                    seSelecciona = true;
-                    dias[pos] = checkBox.getText();
-                    checkBox.setSelected(false);
+    private void llenarHoras(){
+        for (int hora = 6; hora < 22; hora++) {
+            for (int minuto = 0; minuto < 60; minuto += 60) {
+                int horaFin = hora;
+                int minutoFin = minuto + 60;
+                if (minutoFin >= 60) {
+                    horaFin++;
+                    minutoFin = 0;
                 }
-                pos++;
+                String horaInicio = String.format("%02d:%02d", hora, minuto);
+                String horaFinStr = String.format("%02d:%02d", horaFin, minutoFin);
+                String rangoHoras = horaInicio + " - " + horaFinStr;
+                horaInicial.getItems().add(horaInicio);
+                horaFinal.getItems().add(horaFinStr);
             }
         }
-        if(!seSelecciona){
-            Mensajes.mensajeAdvertencia("", "No ha seleccionado ningún día se la semana");
-        }
+    }
+    @FXML
+    private void onComboDiasClick(){
+        String[] dias = new String[]{"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+        comboDias.getItems().clear();
+        comboDias.getItems().setAll(dias);
+    }
+    private void diaSemana(){
+        LocalDate fechaActual = LocalDate.now();
+
+        // Obtén la fecha del próximo lunes
+        LocalDate proximoLunes = fechaActual.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+
+        // Formatea la fecha en el formato "dd-MM-yyyy"
+        String formatoDeseado = proximoLunes.format(java.time.format.DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+
+        // Imprime el resultado
+        System.out.println("Próximo lunes: " + formatoDeseado);
     }
 }
